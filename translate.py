d41315d8a06d725008f327d9e0671abb8e8e6137
capatch-lancpa <dcapatch@cityoflancasterpa.gov>
2024-09-11 23:30:07 -0400

    Create translation into xlsm template file

--- translate.py
+++ translate.py
@@ -1,6 +1,8 @@
 import csv
 import datetime
 
+import openpyxl
+
 
 def material(material) -> str:
     var = [
@@ -163,7 +165,182 @@ def non_field_method(method) -> str:
         return None
 
 
-def translate_csv(input_file, output_file):
+def translate(input_data):
+    output_data = []
+    for row in input_data:
+
+        new_row = []
+        ###################################
+        ## Service Line Basic Information
+        ###################################
+        # Unique Service Line ID (Required)
+        new_row.append(row["ID"])
+
+        # Record Type
+        var = ["Initial", "Update", "Add", "Inactive"]
+        new_row.append(var[0])
+
+        # Date Replacement Completed
+        new_row.append(None)
+
+        # Ownership Type
+        var = ["Joint", "System", "Customer"]
+        new_row.append(var[0])
+
+        # Street Address 1
+        new_row.append(row["Street"])
+
+        # Street Address 2
+        new_row.append(None)
+
+        # City or Township
+        new_row.append(row["City"])
+
+        # Zip Code
+        new_row.append(row["Zipcode"])
+
+        # School?
+        var = ["No", "Yes - Elementary", "Yes - Secondary", "Yes - All Grades"]
+        if row["Building Type"] == "Elementary School":
+            new_row.append(var[1])
+        elif row["Building Type"] == "School Non-Elementary":
+            new_row.append(var[2])
+        else:
+            new_row.append(var[0])
+
+        # Childcare Facility?
+        var = ["No", "Yes"]
+        if row["Building Type"] in [
+            "Day Care",
+            "Residential & In-Home Day Care",
+        ]:
+            new_row.append(var[1])
+        else:
+            new_row.append(var[0])
+
+        ###################################
+        ## System-Owned Portion of Service Line
+        ###################################
+        # Material
+        new_row.append(material(row["Utility Materials"]))
+
+        # Was Material Ever Previously Lead?
+        var = ["Yes", "No", "Not sure"]
+        if row["Utility Previously Lead"] == "Yes":
+            new_row.append(var[0])
+        elif row["Utility Previously Lead"] == "No":
+            new_row.append(var[1])
+        elif row["Utility Previously Lead"] == "Unknown":
+            new_row.append(var[2])
+        else:
+            new_row.append(None)
+
+        # Lead Pigtail, Gooseneck or Connector Upstream?
+        var = ["Yes", "No", "Not sure"]
+        new_row.append(None)
+
+        # Installation Date Range
+        new_row.append(install_date_range(row["Utility Installation Dates"]))
+
+        # Installation Date Specific
+        new_row.append(row["Utility Installation Dates"])
+
+        # "Diameter (in inches)"
+        if row["Utility Diameters"] != "99":
+            new_row.append(row["Utility Diameters"])
+        else:
+            new_row.append(None)
+
+        # "Basis of Material Classification - Non-Field Method"
+        new_row.append(non_field_method(row["Utility Verification Method"]))
+
+        ##### DUPLICATE?!?!
+        # "Basis of Material Classification - Non-Field Method"
+        new_row.append(non_field_method(row["Utility Verification Method"]))
+
+        # "Basis of Material Classification - Field Method"
+        new_row.append(field_method(row["Utility Verification Method"]))
+
+        # Date of Field Verification
+        new_row.append(row["Utility Verification date"])
+
+        # Additional Comments
+        new_row.append(None)
+
+        ###################################
+        ## Customer-Owned Portion of Service Line
+        ###################################
+        # Material
+        new_row.append(material(row["Private Materials"]))
+
+        # Lead Pigtail, Gooseneck or Connector Upstream?
+        var = ["Yes", "No", "Not sure"]
+        new_row.append(None)
+
+        # Installation Date Range
+        new_row.append(install_date_range(row["Private Installation Dates"]))
+
+        # Installation Date Specific
+        new_row.append(row["Private Installation Dates"])
+
+        # "Basis of Material Classification - Non-Field Method"
+        new_row.append(non_field_method(row["Private Verification Method"]))
+
+        ##### DUPLICATE?!?!
+        # "Basis of Material Classification - Non-Field Method"
+        # new_row.append(non_field_method(row["Private Verification Method"]))
+        new_row.append(None)
+
+        # "Basis of Material Classification - Field Method"
+        new_row.append(field_method(row["Private Verification Method"]))
+
+        # Date of Field Verification
+        if row["Private Field Verified"] == "Yes":
+            new_row.append(row["Private Verification Date"])
+        else:
+            new_row.append(None)
+
+        # Additional Comments
+        new_row.append(None)
+
+        ###################################
+        ## Information to Assign Tap Monitoring Tiering
+        ###################################
+        # "Service Line Connected To:"
+        var = [
+            "S) Single family residence",
+            "M) Multi family residence",
+            "O) Building/Other",
+        ]
+        if row["Building Type"] == "Single-Family":
+            new_row.append(var[0])
+        elif row["Building Type"] == "Multi-Family":
+            new_row.append(var[1])
+        else:
+            new_row.append(var[2])
+
+        # POE Treatment Present?
+        var = ["Yes", "No", "Not sure"]
+        new_row.append(None)
+
+        # Interior Building Plumbing Contains Lead Solder?
+        var = ["Yes", "No", "Not sure"]
+        new_row.append(None)
+
+        # Current LCR Sampling Site?
+        var = ["No", "Yes"]
+        new_row.append(None)
+
+        # Check to make sure we have all 34 values
+        if len(new_row) != 34:
+            break
+
+        # Store the modified row
+        output_data.append(new_row)
+    return output_data
+
+
+def translate_to_csv(input_file, output_file):
 
     # Open the input CSV file for reading
     with open(input_file, mode="r", encoding="utf-8") as infile:
@@ -173,180 +350,52 @@ def translate_csv(input_file, output_file):
         with open(output_file, mode="w", newline="", encoding="utf-8") as outfile:
             writer = csv.writer(outfile)
 
-            for row in reader:
-
-                new_row = []
-                ###################################
-                ## Service Line Basic Information
-                ###################################
-                # Unique Service Line ID (Required)
-                new_row.append(row["ID"])
-
-                # Record Type
-                var = ["Initial", "Update", "Add", "Inactive"]
-                new_row.append(var[0])
-
-                # Date Replacement Completed
-                new_row.append(None)
-
-                # Ownership Type
-                var = ["Joint", "System", "Customer"]
-                new_row.append(var[0])
-
-                # Street Address 1
-                new_row.append(row["Street"])
-
-                # Street Address 2
-                new_row.append(None)
-
-                # City or Township
-                new_row.append(row["City"])
-
-                # Zip Code
-                new_row.append(row["Zipcode"])
-
-                # School?
-                var = ["No", "Yes - Elementary", "Yes - Secondary", "Yes - All Grades"]
-                if row["Building Type"] == "Elementary School":
-                    new_row.append(var[1])
-                elif row["Building Type"] == "School Non-Elementary":
-                    new_row.append(var[2])
-                else:
-                    new_row.append(var[0])
-
-                # Childcare Facility?
-                var = ["No", "Yes"]
-                if row["Building Type"] in [
-                    "Day Care",
-                    "Residential & In-Home Day Care",
-                ]:
-                    new_row.append(var[1])
-                else:
-                    new_row.append(var[0])
-
-                ###################################
-                ## System-Owned Portion of Service Line
-                ###################################
-                # Material
-                new_row.append(material(row["Utility Materials"]))
-
-                # Was Material Ever Previously Lead?
-                var = ["Yes", "No", "Not sure"]
-                if row["Utility Previously Lead"] == "Yes":
-                    new_row.append(var[0])
-                elif row["Utility Previously Lead"] == "No":
-                    new_row.append(var[1])
-                elif row["Utility Previously Lead"] == "Unknown":
-                    new_row.append(var[2])
-                else:
-                    new_row.append(None)
-
-                # Lead Pigtail, Gooseneck or Connector Upstream?
-                var = ["Yes", "No", "Not sure"]
-                new_row.append(None)
-
-                # Installation Date Range
-                new_row.append(install_date_range(row["Utility Installation Dates"]))
-
-                # Installation Date Specific
-                new_row.append(row["Utility Installation Dates"])
-
-                # "Diameter (in inches)"
-                if row["Utility Diameters"] != "99":
-                    new_row.append(row["Utility Diameters"])
-                else:
-                    new_row.append(None)
-
-                # "Basis of Material Classification - Non-Field Method"
-                new_row.append(non_field_method(row["Utility Verification Method"]))
-
-                ##### DUPLICATE?!?!
-                # "Basis of Material Classification - Non-Field Method"
-                new_row.append(non_field_method(row["Utility Verification Method"]))
-
-                # "Basis of Material Classification - Field Method"
-                new_row.append(field_method(row["Utility Verification Method"]))
-
-                # Date of Field Verification
-                new_row.append(row["Utility Verification date"])
-
-                # Additional Comments
-                new_row.append(None)
-
-                ###################################
-                ## Customer-Owned Portion of Service Line
-                ###################################
-                # Material
-                new_row.append(material(row["Private Materials"]))
-
-                # Lead Pigtail, Gooseneck or Connector Upstream?
-                var = ["Yes", "No", "Not sure"]
-                new_row.append(None)
-
-                # Installation Date Range
-                new_row.append(install_date_range(row["Private Installation Dates"]))
-
-                # Installation Date Specific
-                new_row.append(row["Private Installation Dates"])
-
-                # "Basis of Material Classification - Non-Field Method"
-                new_row.append(non_field_method(row["Private Verification Method"]))
-
-                ##### DUPLICATE?!?!
-                # "Basis of Material Classification - Non-Field Method"
-                # new_row.append(non_field_method(row["Private Verification Method"]))
-                new_row.append(None)
-
-                # "Basis of Material Classification - Field Method"
-                new_row.append(field_method(row["Private Verification Method"]))
-
-                # Date of Field Verification
-                if row["Private Field Verified"] == "Yes":
-                    new_row.append(row["Private Verification Date"])
-                else:
-                    new_row.append(None)
-
-                # Additional Comments
-                new_row.append(None)
-
-                ###################################
-                ## Information to Assign Tap Monitoring Tiering
-                ###################################
-                # "Service Line Connected To:"
-                var = [
-                    "S) Single family residence",
-                    "M) Multi family residence",
-                    "O) Building/Other",
-                ]
-                if row["Building Type"] == "Single-Family":
-                    new_row.append(var[0])
-                elif row["Building Type"] == "Multi-Family":
-                    new_row.append(var[1])
-                else:
-                    new_row.append(var[2])
-
-                # POE Treatment Present?
-                var = ["Yes", "No", "Not sure"]
-                new_row.append(None)
-
-                # Interior Building Plumbing Contains Lead Solder?
-                var = ["Yes", "No", "Not sure"]
-                new_row.append(None)
-
-                # Current LCR Sampling Site?
-                var = ["No", "Yes"]
-                new_row.append(None)
-
-                if len(new_row) != 34:
-                    break
-
+            data = translate(reader)
+            for row in data:
                 # Write the modified row to the output CSV
-                writer.writerow(new_row)
+                writer.writerow(row)
 
     print(f"Translation complete. Data saved to {output_file}")
+
+
+def translate_to_xlsm(input_csv, input_xlsm, output_xlsm):
+
+    # Open the input CSV file for reading
+    with open(input_csv, mode="r", encoding="utf-8") as infile:
+        reader = csv.DictReader(infile)
+
+        data = translate(reader)
+
+        # Open an existing Excel file or create a new one
+        try:
+            workbook = openpyxl.load_workbook(input_xlsm, keep_vba=True)
+            print(f"File '{input_xlsm}' opened successfully.")
+        except FileNotFoundError:
+            workbook = openpyxl.Workbook()
+            print(f"File '{input_xlsm}' not found, creating a new one.")
+
+        worksheet = workbook["Detailed Inventory"]
+        # E9 starting cell in blank inventory
+        # start_row = 9
+        # start_col = 5
+        curr_row = 9
+        curr_col = 5
+
+        for row in data:
+            for val in row:
+                worksheet.cell(row=curr_row, column=curr_col, value=val)
+                curr_col += 1
+            curr_col = 5
+            curr_row += 1
+
+        workbook.save(output_xlsm)
 
 
 # Example usage
 input_csv = "input.csv"  # Replace with your input CSV file
 output_csv = "translated_output.csv"  # Replace with the output CSV file
-translate_csv(input_csv, output_csv)
+# translate_csv(input_csv, output_csv)
+
+input_xlsm = "SERVICE_LINE_INVENTORY_FORM.xlsm"
+output_xlsm = "output.xlsm"
+translate_to_xlsm(input_csv, input_xlsm, output_xlsm)
